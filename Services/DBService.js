1964013e3f0b49f6e827b86f0df59f2699e48ac5
Steph-ski <stephaniejohns57@gmail.com>
2023-04-25 15:22:58 +0100

    Services directory and DBService added

---
+++ Services/DBService.js
@@ -0,0 +1,11 @@
+const {MongoClient} = require("mongodb");
+const url = "mongodb://root:password@localhost:27017"
+
+const DBService = async () =>  {
+    const connection = await MongoClient.connect(url)
+    const db = connection.db('toppings')
+    const collection = db.collection('toppings')
+    return collection
+}
+
+module.exports = DBService
