c92574a7fe70a1ea4b033321c6345b4d1c86bb97
Steph-ski <stephaniejohns57@gmail.com>
2023-04-25 17:07:31 +0100

    comment added to aid Yas and Nick

--- Config/routes.js
+++ Config/routes.js
@@ -1,6 +1,7 @@
 const ToppingsController = require('../Controllers/ToppingsController')
 
 const routes = (app) => {
+    //this can be deleted, was just using this as a test to the URL
     app.get('/', (req, res) => {
         res.send('hello world')
     })
