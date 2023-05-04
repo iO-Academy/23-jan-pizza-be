669948a43311d9cb82e560d9b92ce988dc0c66fe
barebarebears <nick.hackland@gmail.com>
2023-05-04 13:10:52 +0100

    added function to add vote to topping

--- Config/routes.js
+++ Config/routes.js
@@ -2,6 +2,10 @@ const ToppingsController = require('../Controllers/ToppingsController')
 
 const routes = (app) => {
     app.get('/toppings', ToppingsController.getAllToppings)
+
+    app.put('/toppings', ToppingsController.addVoteToTopping)
 }
+
+
 
 module.exports = routes
--- Controllers/ToppingsController.js
+++ Controllers/ToppingsController.js
@@ -1,5 +1,6 @@
 const DBService = require("../Services/DBService")
 const JsonResponseService = require('../Services/JsonResponseService')
+const {ObjectId } = require('mongodb')
 
 const getAllToppings = async (req, res) => {
     let queryFilter = {}
@@ -9,4 +10,17 @@ const getAllToppings = async (req, res) => {
     res.json(JsonResponseService('all toppings displayed', data))
 }
 
+const addVoteToTopping = async (req, res) => {
+    let id = new ObjectId(req.body.id)
+
+    const collection = await DBService('toppings')
+    let vote = await collection.updateOne({_id: id}, {$inc: {votes: 1}})
+    if(vote.modifiedCount !== null) {
+        res.json(JsonResponseService('it worked!'))
+    } else {
+        res.json(JsonResponseService('it did not work'))
+    }
+}
+
 exports.getAllToppings = getAllToppings
+exports.addVoteToTopping = addVoteToTopping
