78b80443600feb6fae487080420d382dac351968
Steph-ski <stephaniejohns57@gmail.com>
2023-05-10 14:11:07 +0100

    adding in Atlas MongoDB connection

--- Services/DBService.js
+++ Services/DBService.js
@@ -1,8 +1,17 @@
-const {MongoClient} = require("mongodb");
-const url = "mongodb://root:password@localhost:27017"
+const {MongoClient, ServerApiVersion} = require("mongodb");
+const uri = "mongodb+srv://<username>:<password>@cluster0.n5j2whs.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 const DBService = async () =>  {
-    const connection = await MongoClient.connect(url)
+    const connection = await client.connect()
     const db = connection.db('toppings')
     const collection = db.collection('toppings')
     return collection
